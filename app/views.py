c819494c01de5d02e942920a7c3672f4682964f0
TheCoolLess <nathanjiang@live.com>
2016-11-15 14:01:12 -0500

    made basic flask/html files

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def homepage():
-	return "Hello World!"
+	return render_template('index.html')
+
+@app.route('/resume/')
+def resumepage():
+	return render_template('resume.html')
