1ff102dd7409826411e77052bd5053479b106f02
TheCoolLess <nathanjiang@live.com>
2016-12-03 22:21:10 -0500

    commit for server

--- app/views.py
+++ app/views.py
@@ -8,3 +8,11 @@ def homepage():
 @app.route('/resume/')
 def resumepage():
 	return render_template('resume.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404;
+
+@app.errorhandler(500)
+def servor_error(e):
+	return render_template('500.html'), 500;
