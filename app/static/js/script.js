a14435980117c6181f5b53e1b2829373abd38080
TheCoolLess <nathanjiang@live.com>
2017-01-17 00:13:06 -0500

    Final revision for now

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,4 @@
+/*
 (function() {
   var cards = document.querySelectorAll(".card.effect__click");
   for ( var i  = 0, len = cards.length; i < len; i++ ) {
@@ -12,4 +13,34 @@
       c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
     });
   }
-})();
+})(); */
+
+$(document).ready(function() {
+  
+  var stack1 = $('#stack1');
+  
+  stack1.removeClass('start');
+  
+  stack1.hammer().on('tap', function(event) {
+    stack1.addClass('hover');
+    event.stopPropagation();
+  });
+  
+  
+  $(document).hammer().on('tap', function(event) {
+    stack1.removeClass('hover');
+    $('.card').removeClass('hover');
+  });
+  
+  $('.card').hammer().on('tap', function(event) {
+      $('.card').removeClass('hover');
+      $(this).addClass('hover');
+  });
+});
+
+function hover(element) {
+    element.setAttribute('src', 'https://s29.postimg.org/57qs862xz/Hacker_Rank_logo.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'https://s30.postimg.org/gxuty67jl/Hacker_Rank_logo.png');
+}
